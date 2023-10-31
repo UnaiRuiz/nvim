81eec75bfa39f97369c12c71488aff00c549ab10
Unai Ruiz <urs97sestao@gmail.com>
2023-10-31 09:15:27 +0100

    Add custom flake8 args
    - Add custom flake8 args when .flake8 is not defined

--- lua/unai/plugins/formatting.lua
+++ lua/unai/plugins/formatting.lua
@@ -20,12 +20,12 @@ return {
       },
     })
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+    vim.keymap.set({ "n", "v" }, "<leader>lf", function()
       conform.format({
         lsp_fallback = false,
         async = false,
         timeout_ms = 1000,
       })
-    end, { desc = "Format file or range (in visual mode)" })
+    end, { desc = "Format document or range (in visual mode)" })
   end,
 }
--- lua/unai/plugins/linting.lua
+++ lua/unai/plugins/linting.lua
@@ -11,8 +11,13 @@ return {
       json = { "jsonlint" },
     }
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    if vim.fn.filereadable(vim.fn.getcwd() .. "/.flake8") ~= 1 then
+      table.insert(lint.linters.flake8.args, "--max-line-length=100")
+      table.insert(lint.linters.flake8.args, "--ignore=E203,W503")
+      table.insert(lint.linters.flake8.args, "--extend-select=W504")
+    end
 
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
@@ -20,8 +25,8 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "<leader>li", function()
+    vim.keymap.set("n", "<leader>ll", function()
       lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
+    end, { desc = "Lint Document" })
   end,
 }
