d19c6f22946b49a1b3346075c75d8c0abb29874f
Unai Ruiz <urs97sestao@gmail.com>
2024-02-22 11:35:27 +0100

    Add oil plugin

---
+++ lua/unai/plugins/oil.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {
+    view_options = {
+      show_hidden = true,
+      sort = {
+        { "type", "asc" },
+        { "name", "asc" },
+      },
+    },
+  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<leader>o", "<cmd>Oil --float<cr>", desc = "Open oil buffer" },
+  },
+}
