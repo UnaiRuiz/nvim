f91ecde37b54b72a764c0e2d0a9d89b6cc3ab078
Unai Ruiz <urs97sestao@gmail.com>
2024-07-08 10:10:40 +0200

    Add custom snippets

--- lua/unai/plugins/completion.lua
+++ lua/unai/plugins/completion.lua
@@ -21,7 +21,6 @@ return {
 
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
-    require("luasnip.loaders.from_vscode").lazy_load()
     local check_backspace = function()
       local col = vim.fn.col(".") - 1
       return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
